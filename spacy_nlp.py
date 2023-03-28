e95ac36fc5a76567692af599ea03d0497fae4c0c
katgrubbs14 <katgrubbs14@gwmail.gwu.edu>
2023-03-28 14:48:20 -0400

    dd

---
+++ spacy_nlp.py
@@ -0,0 +1,9 @@
+import spacy
+
+
+
+if __name__ == '__main__':
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp("Apple is looking at buying U.K. startup for $1 billion")
+    for token in doc:
+        print(token.text, token.pos_, token.dep_)
