9dd6dc6a01187a9fd3dfed94d7a571661a244093
graceli01 <graceeliquq@gmail.com>
2023-04-11 14:57:06 -0400

    grace - api code

--- test_api.py
+++ test_api.py
@@ -1,51 +1,64 @@
-#pip install shiny
-#pip install waiter
-#shiny run --reload
-from doctest import OutputChecker
-from shiny import ui, render, App
 import http.client, urllib.parse
 import json
 import pandas as pd
 
+conn = http.client.HTTPConnection('api.mediastack.com')
 
-app_ui = ui.page_fluid(
-    ui.layout_sidebar(
-        ui.panel_sidebar(
-            #ui.input_select("website", "Choose a website:", {"independent" : "Kyiv Independent", "inform":"UKInform", "pravda":"Pravda", "[website_name]":"website_name"}),
-            ui.input_select("website", "Choose a website:", {"API" : "Mediastack", "pravda":"Pravda"}),
-            #ui.input_radio_buttons("use_file", "Use pre-existing data?", file_choices),
-            ui.input_action_button("start", "Start"),
-            ui.download_button("download_final", "Download", class_="btn-primary")
-        ),
-        ui.panel_main(
-            #ui.output_text_verbatim("scrape", placeholder=True)
-            ui.output_table("scrape_mediastack")
-        )
-    )
-)
-
-def server(input, output, session):
-    @output
-    @render.table
-    @reactive.event(input.start)
-    def scrape_mediastack():
-        base_url = http.client.HTTPConnection('api.mediastack.com')
-
-        params = urllib.parse.urlencode({
-            'access_key': 'd9babc2a947d03f9b887715a6df56a7a',
-            'categories': 'general',
-            'keywords': 'Ukraine Russian',
-            'languages': 'ar, en, fr, ru, zh',
-            'sort': 'published_desc',
-            'limit': 10,
+params = urllib.parse.urlencode({
+    'access_key': 'd9babc2a947d03f9b887715a6df56a7a',
+    'categories': 'general',
+    'keywords': 'Ukraine Russian',
+    'languages': 'ar, en, fr, ru, zh',
+    'sort': 'published_desc',
+    'limit': 100,
     })
-    
-        conn.request('GET', "/v1/news?{}".format(params))
 
-        res = conn.getresponse()
-        data = res.read()
-        return data.decode('utf-8')
-    
-    
+conn.request('GET', "/v1/news?{}".format(params))
+res = conn.getresponse()
+data = res.read()
 
-app = App(app_ui, server)
+import json
+response_dict = json.loads(data.decode('utf-8'))
+for article in response_dict['data']:
+    print(article['author'])
+    print(article['title'])
+    print(article['description'])
+    print(article['url'])
+    print(article['source'])
+    print(article['published_at'])
+    print('-' * 50)
+
+# create dataframe to store output
+scraped_data = []
+
+for article in response_dict['data']:
+    # Store the article data in variables
+    author = article['author']
+    title = article['title']
+    description = article['description']
+    url = article['url']
+    source = article['source']
+    published_at = article['published_at']
+
+    # Append the scraped data to the list
+    scraped_data.append({
+        'author': author,
+        'title': title,
+        'description': description,
+        'url': url,
+        'source': source,
+        'published_at': published_at
+    })
+
+df_API = pd.DataFrame(scraped_data)
+#df = df[df['title'].isin(['Ukrain', 'Russian'])]
+
+df_API.head() #display dataframe
+
+# update the date format
+# Convert the 'published_at' column to datetime objects and then format them into yyyy-mm-dd strings
+df_API['published_at'] = pd.to_datetime(df_API['published_at']).dt.strftime('%Y-%m-%d')
+
+# Assuming your dataframe is called df_API
+df_API.to_csv('API_news.csv', encoding = 'utf-8-sig',index=False)
+
